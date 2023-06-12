95e3ef43e27f4cb0f1947df3884f91c7f0ceb925
klam2k20 <klam2k17@gmail.com>
2023-06-12 00:24:16 -0400

    Update token and block queries

--- src/apollo/queries.ts
+++ src/apollo/queries.ts
@@ -1,14 +1,67 @@
 import { gql } from '@apollo/client';
 
-export const TOP_TOKENS = gql`
-  query GetTokens {
+/**
+ * Get block number from timestamp
+ */
+export const BLOCK = gql`
+  query GetBlock($timestampFrom: Int!, $timestampTo: Int!) {
+    blocks(
+      first: 1
+      orderBy: timestamp
+      orderDirection: asc
+      where: { timestamp_gte: $timestampFrom, timestamp_lt: $timestampTo }
+    ) {
+      number
+    }
+  }
+`;
+
+/**
+ * Get top 100 token ids ranked by TVL(USD)
+ */
+export const TOP_TOKEN_IDS = gql`
+  query GetTokenIds {
     tokens(first: 100, orderBy: totalValueLockedUSD, orderDirection: desc) {
       id
-      symbol
+    }
+  }
+`;
+
+/**
+ * Get current top 100 tokens ranked by TVL(USD)
+ */
+export const CURRENT_TOKEN_DATA = gql`
+  query GetCurrentTokenData($tokenIds: [String]!) {
+    tokens(where: { id_in: $tokenIds }, orderBy: totalValueLockedUSD, orderDirection: desc) {
+      id
       name
+      symbol
       totalValueLockedUSD
-      tokenDayData(first: 2, orderBy: date, orderDirection: desc) {
-        date
+      volumeUSD
+      tokenDayData(first: 1, orderBy: date, orderDirection: desc) {
+        priceUSD
+      }
+    }
+  }
+`;
+
+/**
+ * Get top 100 tokens ranked by TVL(USD) for a specific block number
+ */
+export const BLOCK_TOKEN_DATA = gql`
+  query GetBlockTokenData($tokenIds: [String]!, $blockNumber: Int!) {
+    tokens(
+      where: { id_in: $tokenIds }
+      block: { number: $blockNumber }
+      orderBy: totalValueLockedUSD
+      orderDirection: desc
+    ) {
+      id
+      name
+      symbol
+      totalValueLockedUSD
+      volumeUSD
+      tokenDayData(first: 1, orderBy: date, orderDirection: desc) {
         priceUSD
       }
     }
