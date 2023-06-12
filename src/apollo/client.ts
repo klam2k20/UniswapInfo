1f79c637eebb9d9de5ab81bfca97a6e0db79dd47
klam2k20 <klam2k17@gmail.com>
2023-06-12 00:23:50 -0400

    Update apollo client for uniswap and block endpoint

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,34 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
+
+/**
+ * Endpoint to query Uniswap Subgraph V3 for token, pool and
+ * transaction data
+ */
+const uniswapEndpoint = new HttpLink({
+  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
+});
+
+/**
+ * Endpoint to query for block number from given timestamp - needed to
+ * get token and pool data for a specific time
+ */
+const blockEndpoint = new HttpLink({
+  uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks'
+});
 
 export const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
-  cache: new InMemoryCache()
+  link: ApolloLink.split(
+    (op) => op.getContext().clientName === 'uniswap',
+    uniswapEndpoint,
+    blockEndpoint
+  ),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache'
+    },
+    query: {
+      fetchPolicy: 'no-cache'
+    }
+  }
 });
